2ef6afa7286530b59daa9cb4f5709f08822c6000
EricWXY <ericwxy@foxmail.com>
2024-06-18 18:01:32 +0800

    fix: add vitest.config to hooks

---
+++ packages/hooks/vitest.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/true/coverage/**",
+      "**/coverage/**",
+    ]
+  },
+});
